5404084b563d149c64f4728472336c9fba6ec9d3
rehman <rehman@rehmans-MacBook-Air.local>
2025-01-16 09:59:05 +0530

    n

--- Filewrite.py
+++ Filewrite.py
@@ -1,7 +1,7 @@
 text = input("Enter text to write to the file: ")
-file_path = input("Enter the name of the file to write to: ")
+file_name = input("Enter the name of the file to write to: ")
 
-with open(file_path, 'w') as file:
+with open(file_name, 'w') as file:
     file.write(text)
 
-print(f"Text written to {file_path}")
+print(f"Text written to {file_name}")
